b8265e575e49e84d7dc25f47ddb645d04e087be4
Nyasha Bryan <nyashabryan23@gmail.com>
2019-03-29 22:55:22 +0200

    Constructors and destructors of node and tree

--- huffmantree.cpp
+++ huffmantree.cpp
@@ -1,2 +1,13 @@
 #include "huffmantree.h"
 
+/**
+ * Huffman Tree Class Methods
+ */
+
+/**
+ * Huffman Tree class destructor
+ */
+KTMNYA001::HuffmanTree::~HuffmanTree(){
+    root = nullptr;
+}
+
--- huffmantree.h
+++ huffmantree.h
@@ -1,14 +1,37 @@
+#ifndef HUFFMANNODE_H
+#include <memory>
+
 namespace KTMNYA001{
 
+    class HuffmanNode{
+
+        /**
+         * The left and right pointers
+         */
+        std::shared_ptr<KTMNYA001::HuffmanNode> left;
+        std::shared_ptr<KTMNYA001::HuffmanNode> right;
+
+        char letter;
+        int frequency;
+
+        // Constructor
+        HuffmanNode();
+        // Deconstructor
+        ~HuffmanNode();
+
+    };
+
     class HuffmanTree{
+
+        std::shared_ptr<HuffmanNode> root;
 
         // Constructor
         HuffmanTree();
         //Destructor
         ~HuffmanTree();
-    };
-
-    class HuffmanNode{
 
     };
+
 }
+
+#endif
